63a83508a0556ef3cd04de6b6a4248ad895a1672
Samuel Sato <samuelsato2009@gmail.com>
2025-06-07 01:45:48 -0300

    feat: Adição projeto inicial front

---
+++ front/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'front';
+}
---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
+
+export const routes: Routes = [
+    {path: 'department', component: DepartmentComponent}
+];
+
